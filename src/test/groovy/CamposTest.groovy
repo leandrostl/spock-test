166068aedd5124633891ab22ee29338cd43eed27
Leandro Lima <leandro.stlima@gmail.com>
2020-07-09 00:16:24 -0300

    Separa melhor os testes.

--- src/test/groovy/CamposTest.groovy
+++ src/test/groovy/CamposTest.groovy
@@ -1,21 +1,9 @@
 import spock.lang.Shared
 import spock.lang.Specification;
 
-class Teste extends Specification {
+class CamposTest extends Specification {
 	def campo = 5
 	@Shared campo2 = 3
-
-	def "o maior valor entre dois números"() {
-		expect:
-		Math.max(a, b) == c
-
-		where:
-		a | b | c
-		1 | 2 | 2
-		5 | 2 | 5
-		0 | 1 | 1
-		-1 | 1 | 1
-	}
 	
 	def "alterando o valor do campo"() {
 		campo = 6
---
+++ src/test/groovy/MaxTest.groovy
@@ -0,0 +1,16 @@
+import spock.lang.Shared
+import spock.lang.Specification;
+
+class MaxTest extends Specification {
+	def "o maior valor entre dois números"() {
+		expect:
+		Math.max(a, b) == c
+
+		where:
+		a | b | c
+		1 | 2 | 2
+		5 | 2 | 5
+		0 | 1 | 1
+		-1 | 1 | 1
+	}
+}
