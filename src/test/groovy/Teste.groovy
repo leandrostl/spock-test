0fbaa4f8f8059a8e5d5a56d5917b8ce127171c40
Leandro Lima <leandro.stlima@gmail.com>
2020-07-08 22:02:20 -0300

    Corrige de dependências.

--- src/test/groovy/Teste.groovy
+++ src/test/groovy/Teste.groovy
@@ -1,17 +1,34 @@
-import spock.lang.Specification
+import spock.lang.Shared
+import spock.lang.Specification;
 
 class Teste extends Specification {
-	
+	def campo = 5
+	@Shared campo2 = 3
+
 	def "o maior valor entre dois números"() {
 		expect:
 		Math.max(a, b) == c
-	
+
 		where:
 		a | b | c
 		1 | 2 | 2
 		5 | 2 | 5
 		0 | 1 | 1
 		-1 | 1 | 1
-	  }
+	}
 	
+	def "alterando o valor do campo"() {
+		campo = 6
+		campo2 = 4
+		
+		expect:
+		campo2 == 4
+		campo == 6
+	}
+	
+	def "mantem o valor do campo inalterado"() {
+		expect:
+		campo2 == 4
+		campo == 5
+	}
 }
